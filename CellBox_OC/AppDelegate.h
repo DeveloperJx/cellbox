54d54dbd4a53ac703d3683835fab5c219fbc57f1
Jx <jx@JxdeMac.local>
2015-07-08 17:19:33 +0800

    Initial Commit

---
+++ CellBox_OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CellBox_OC
+//
+//  Created by Jx on 15/7/8.
+//  Copyright (c) 2015年 Jx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CellBox_OC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CellBox_OC
+//
+//  Created by Jx on 15/7/8.
+//  Copyright (c) 2015年 Jx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
