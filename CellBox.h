c1fb5b96e9d4a6654c5255489530fa8f7e9d68d8
Jx <741258063@qq.com>
2015-07-13 16:57:52 +0800

    firstcommit

---
+++ CellBox.h
@@ -0,0 +1,13 @@
+//
+//  CellBox.h
+//  CellBox_OC
+//
+//  Created by Jx on 15/7/9.
+//  Copyright © 2015年 Jx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CellBox : NSObject
+
+@end
---
+++ CellBox.m
@@ -0,0 +1,13 @@
+//
+//  CellBox.m
+//  CellBox_OC
+//
+//  Created by Jx on 15/7/9.
+//  Copyright © 2015年 Jx. All rights reserved.
+//
+
+#import "CellBox.h"
+
+@implementation CellBox
+
+@end
